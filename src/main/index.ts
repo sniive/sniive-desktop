06f5a27facfd51bee1ae341de44fe66c729bdcf1
eyusd <eyusd.gaming@gmail.com>
2024-06-12 16:05:10 +0200

    remove update

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,7 +5,6 @@ import { ChildProcessWithoutNullStreams } from 'child_process'
 import { connectIpc } from './ipcConnect'
 import { createWindow } from './createWindow'
 import { Auth, killScriptSubprocess, matchDeepLink } from './utils'
-import { update } from './update'
 
 let scriptSubprocess: ChildProcessWithoutNullStreams | null
 let auth: Auth = { spaceName: '', access: '' }
@@ -47,7 +46,7 @@ if (!gotTheLock) {
     }
 
     const mainWindow = createWindow()
-    update(mainWindow)
+    //update(mainWindow)
     connectIpc({ mainWindow, scriptSubprocess, killScriptSubprocess, auth, app })
 
     app.on('second-instance', (_, commandLine: string[]) => {
